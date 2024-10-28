36509778b4c655c2f78870c0831766335ad69dda
samchi <seyoung0314@gmail.com>
2024-10-28 11:24:24 +0900

    js-1-3-데이터타입 기본 학습

---
+++ 1-3-데이터타입/1-basic-type.js
@@ -0,0 +1,12 @@
+let age = 20; // => 20.0
+console.log(typeof age);
+
+// js는 숫자를 전부 실수로 판단
+let textRate = 0.15;
+console.log(typeof textRate);
+console.log(textRate);
+
+console.log(age / 0); // infinity
+
+let name = '세영';
+console.log(age * name); // NaN (Not a Number) 
