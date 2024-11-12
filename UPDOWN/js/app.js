3e1e9eadf18fd1bb0ea412343988cd81dcd66a21
samchi <seyoung0314@gmail.com>
2024-11-12 18:49:44 +0900

    updown게임 난이도 추가

--- UPDOWN/js/app.js
+++ UPDOWN/js/app.js
@@ -2,16 +2,49 @@
 // 1~100 까지의 랜덤정수 생성
 const randomNumber = Math.floor(Math.random() * 100 + 1);
 
+// 선택된 레벨 변수
+let level = null;
+
+// 레벨에 따른 횟수 변수
+let count = 0;
+
 console.log(randomNumber);
 
 const $numbers = document.getElementById("numbers");
 const $upIcon = document.getElementById("up");
 const $downIcon = document.getElementById("down");
 const $finish = document.getElementById("finish");
+const $levelBtn = document.querySelectorAll(".level-btn");
+const $modal = document.getElementById('modalOverlay');
+const $modalEnd = document.querySelector('.gameover');
+const $count = document.getElementById('count');
 
+// 난이도 선택
+function selectedLevel(e){
+  level = e.target.dataset.level;
+  console.log(`level : ${level}`);
+  $modal.style.display = 'none';
+
+  // 난이도 선택에 따른 횟수 저장
+  switch(level){
+    case 'high' : count = 3; break;
+    case 'middle' : count = 5; break;
+    case 'low' : count = 10; break;
+  }
+  
+  $count.textContent = count;
+  
+  console.log(`count : ${count}`);
+  
+  
+}
+
+// 초기 숫자 세팅 1~100
 function initialSet() {
+  
+  // 일단 초기화를 해준다
   $numbers.innerHTML = "";
-  // 초기 숫자 세팅 1~100
+  
   for (let i = 1; i < 101; i++) {
     const $number = document.createElement("div");
     $number.textContent = i;
@@ -27,26 +60,36 @@ initialSet();
 // 숫자를 눌렀을 떄
 function numberClick(e) {
   const clickedNumberIndex = +e.target.dataset.index;
-
+  
+  // 기회가 다 소진됬을 경우
+  if(count === 1){
+    $count.textContent = 0;
+    $upIcon.classList.remove("selected");
+    $downIcon.classList.remove("selected");
+    $finish.textContent = 'Game Over...';
+    $finish.classList.add("show");
+    return;
+  }
+  
   // 15가 정답 18을 고르면 1~18
   // 고른 숫자가 정답 숫자보다 클 때 down
   if (clickedNumberIndex > randomNumber) {
     $downIcon.classList.add("selected");
     $upIcon.classList.remove("selected");
-
+    
     for (let i = clickedNumberIndex + 1; i <= 100; i++) {
       const $number = document.getElementById(`num${i}`);
       if ($number) {
         $number.remove();
       }
     }
-
+    
     // 정답 10 내가 5를 고름 5~100
     // 고른 숫자가 정답 숫자보다 작을 때 up
   } else if (clickedNumberIndex < randomNumber) {
     $upIcon.classList.add("selected");
     $downIcon.classList.remove("selected");
-
+    
     for (let i = 1; i <= clickedNumberIndex; i++) {
       const $number = document.getElementById(`num${i}`);
       console.log($number);
@@ -60,6 +103,12 @@ function numberClick(e) {
     $downIcon.classList.remove("selected");
     $finish.classList.add("show");
   }
+  count--;
+  $count.textContent = count;
 }
+
 // 클릭 이벤트
 $numbers.addEventListener("click", numberClick);
+$levelBtn.forEach(btn=>{
+  btn.addEventListener("click",selectedLevel);
+})
